3f9088fa35ad25bfe7f14c03c198c529e7f3c603
andres.barros <andres.barros@signeblock.com>
2022-02-21 10:28:57 +0100

    adding comments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,10 @@
 const deploy = async () => {
+  // ethers is available in the global scope from ethers library
   const [deployer] = await ethers.getSigners();
 
   console.log('Deployer:', deployer.address);
 
+  //getContractFactory take the information from the compilation cache
   const AndresPunks = await ethers.getContractFactory('AndresPunks');
   const deployed = await AndresPunks.deploy();
 
